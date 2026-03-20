4720d9f204135a4a2c87f3d94a7af5f3c5b15c43
rodrigo <rodrigojosehong@gmail.com>
2026-03-19 19:19:01 -0600

    Add files via upload

---
+++ Association.java
@@ -0,0 +1,27 @@
+public class Association<K extends Comparable<K>, V> implements Comparable <Association<K, V>> {
+    private K key;
+    private V value;
+
+    public Association(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey(){
+        return key;
+    }
+
+    public V getValue(){
+        return value;
+    }
+
+    @Override
+    public int compareTo(Association<K, V> other) {
+        return this.key.compareTo(other.getKey());
+    }
+
+    @Override
+    public String toString() {
+        return "(" + key + ", " + value + ")";
+    }
+}
